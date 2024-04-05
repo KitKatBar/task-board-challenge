c1317c07feda109218279c1c2cca7594192c22dd
Katriel Chiu <katriel_chiu@msn.com>
2024-04-04 23:25:32 -0700

    added comments to the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,18 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-// Todo: create a function to generate a unique task id
-// Random ID generator - TODO check
+/*
+  Here we use a tool called `crypto` to generate a random id for our task.
+  This is a unique identifier that we can use to find the task in the array.
+  `crypto` is a built-in module that we can use in the browser and Nodejs.
+*/
 function generateTaskId() {
     return crypto.randomUUID();
 }
 
-// Todo: create a function to create a task card
-// Create task elements - TODO
+// Creates a task card from the information passed in `task` parameter and returns it
 function createTaskCard(task) {
-
+    // Create our element tags here and append the appropiate classes, attributes and text
     const cardEl = $('<div>')
         .addClass('card task-card draggable my-3')
         .attr('data-task-id', task.id);
@@ -36,14 +38,19 @@ function createTaskCard(task) {
         .text('Delete')
         .attr('data-task-id', task.id);
 
+    // Button on the task will handle delete events
     buttonEl.on('click', handleDeleteTask);
 
-    // ? Sets the card background color based on due date. Only apply the styles if the dueDate exists and the status is not done.
+    /*
+      Sets the card background color based on due date.
+      Only apply the styles if the dueDate exists and the status is not done.
+    */
     if (task.dueDate && task.status !== 'done') {
+        // Use the built in Day.js library to get time stamps and format it
         const now = dayjs();
         const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
 
-        // ? If the task is due today, make the card yellow. If it is overdue, make it red.
+        // If the task is due today, make the card yellow. If it is overdue, make it red.
         if (now.isSame(taskDueDate, 'day')) {
             cardEl.addClass('bg-warning text-white');
         }
@@ -54,24 +61,22 @@ function createTaskCard(task) {
         }
     }
 
-    // TODO: Append the card description, card due date, and card delete button to the card body.
+    // Gather all the elements created above and append them to the correct elements
     cardBodyEl.append(cardDescriptionEl, cardDueDateEl, buttonEl);
-    // TODO: Append the card header and card body to the card.
     cardEl.append(cardHeaderEl, cardBodyEl);
-    // ? Return the card so it can be appended to the correct lane.
+
+    // Return the card so it can be appended to the correct lane
     return cardEl;
 }
 
-// Todo: create a function to render the task list and make cards draggable
-// Local storage parse - TODO check
-// List appending - TODO check
-// Draggable feature - TODO check
+// Function to render the task list display and make cards draggable
 function renderTaskList() {
+    // If no tasks were retrieved from localStorage, assign tasks to a new empty array to push to later
     if (!taskList) {
         taskList = [];
     }
 
-    // ? Empty existing project cards out of the lanes
+    // Empty existing task cards out of the lanes
     const todoList = $('#todo-cards');
     todoList.empty();
 
@@ -81,6 +86,7 @@ function renderTaskList() {
     const doneList = $('#done-cards');
     doneList.empty();
 
+    // Loop through tasks and create task cards for each status
     for (let task of taskList) {
         if(task.status === 'to-do') {
             todoList.append(createTaskCard(task));
@@ -95,17 +101,28 @@ function renderTaskList() {
         }
     }
     
-      // ? Use JQuery UI to make task cards draggable
+    // Use JQuery UI to make task cards draggable
     $('.draggable').draggable({
         opacity: 0.7,
         zIndex: 100,
-        // ? This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
+        /*
+          This is the function that creates the clone of the card that is dragged.
+          This is purely visual and does not affect the data.
+        */
         helper: function (e) {
-            // ? Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
+            /*
+              Check if the target of the drag event is the card itself or a child element.
+              If it is the card itself, clone it, otherwise find the parent card
+              that is draggable and clone that.
+            */
             const original = $(e.target).hasClass('ui-draggable')
                 ? $(e.target)
                 : $(e.target).closest('.ui-draggable');
-            // ? Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
+            /*
+              Return the clone with the width set to the width of the original card.
+              This is so the clone does not take up the entire width of the lane.
+              This is to also fix a visual bug where the card shrinks as it's dragged to the right.
+            */
             return original.clone().css({
                 width: original.outerWidth(),
             });
@@ -113,15 +130,17 @@ function renderTaskList() {
     });
 }
 
-// Todo: create a function to handle adding a new task
-// Add new task - TODO check
+// Adds a task to local storage and prints the task data
 function handleAddTask(event){
-    //event.preventDefault();
+    // Had to remove this in order for form validation to work
+    // event.preventDefault();
 
+    // Read user input from the form
     const taskTitle = $('#taskTitle').val();
     const taskDueDate = $('#taskDueDate').val();
     const taskDescription = $('#taskDescription').val();
 
+    // Assign the inputs to the appropriate fields in the task object
     const task = {
         id: generateTaskId(),
         title: taskTitle,
@@ -130,22 +149,30 @@ function handleAddTask(event){
         status: 'to-do'
     }
 
+    // Push the new task to the array
     taskList.push(task);
+
+    // Save the updated tasks array to localStorage
     localStorage.setItem('tasks', JSON.stringify(taskList));
+
+    // Print task data back to the screen
     renderTaskList();
 
+    // Clear the form inputs
     $('#taskTitle').val('');
     $('#taskDueDate').val('');
     $('#taskDescription').val('');
 }
 
-// Todo: create a function to handle deleting a task
+// Removes a task from local storage and prints the task data back to the page
 function handleDeleteTask(event) {
+    // Prevent page from reloading
     event.preventDefault();
 
+    // Grab the task id by the attribute
     const taskId = $(this).attr('data-task-id');
-    console.log(`taskId = ${taskId}`);
-    // TODO: Loop through the projects array and remove the project with the matching id.
+
+    // Remove task from the array
     for (let task of taskList) {
         console.log(`task.id = ${task.id}`);
         if (task.id === taskId) {
@@ -155,121 +182,59 @@ function handleDeleteTask(event) {
         }
     }
 
-    // ? We will use our helper function to save the projects to localStorage
+    // Save the tasks to localStorage
     localStorage.setItem('tasks', JSON.stringify(taskList));
 
-    // ? Here we use our other function to print projects back to the screen
+    // Here we use our other function to print tasks back to the screen
     renderTaskList();
 }
 
-// Todo: create a function to handle dropping a task into a new status lane
+/*
+  This function is called when a card is dropped into a lane. 
+  It updates the status of the task and saves it to localStorage. 
+  You can see this function is called in the `droppable` method below.
+*/
 function handleDrop(event, ui) {
-    // ? Read projects from localStorage
-    // ? Get the project id from the event
+    // Get the task id from the event
     const taskId = ui.draggable[0].dataset.taskId;
 
-    // ? Get the id of the lane that the card was dropped into
+    // Get the id of the lane that the card was dropped into
     const newStatus = event.target.id;
 
     for (let task of taskList) {
-        // ? Find the project card by the `id` and update the project status.
+        // Find the task card by the `id` and update the task status
         if (task.id === taskId) {
             task.status = newStatus;
         }
     }
-    // ? Save the updated projects array to localStorage (overwritting the previous one) and render the new project data to the screen.
+
+    // Save the updated tasks array to localStorage (overwritting the previous one)
     localStorage.setItem('tasks', JSON.stringify(taskList));
+    // Render the new task data to the screen
     renderTaskList();
 }
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-// Render the task list - TODO check
-// Add event listeners - TODO
-// Make lanes droppable - TODO check
-// Due date field date pick - DONE
+/*
+  When the page loads, render the task list to the screen and make the lanes droppable.
+  An event listener is included for form submission, which will create a task.
+  Initializes the date picker for the due date field.
+*/
 $(document).ready(function () {
-    // ? Print project data to the screen on page load if there is any
+    // Print task data to the screen on page load if there is any
     renderTaskList();
-
-    // ? Add event listener to the form element, listen for a submit event, and call the `handleProjectFormSubmit` function.
-    // Form submit - works, TODO logic
-    // $('#taskForm').validate({
-    //     rules: {
-    //       taskTitle: {
-    //         required: true
-    //       },
-    //       taskDueDate: {
-    //         required: true
-    //       },
-    //       taskDescription: {
-    //         required: true
-    //       },
-    //       action: "required"
-    //     },
-    //     messages: {
-    //         taskTitle: {
-    //         required: "Please enter some data",
-    //       },
-    //       taskDueDate: {
-    //         required: "Please enter some data",
-    //       },
-    //       taskDescription: {
-    //         required: "Please enter some data",
-    //       },
-    //       action: "Please provide some data"
-    //     },
-    //     //errorElement: $('#taskForm').addClass('was-validated')
-    //   }).addClass('was-validated').on('submit', handleAddTask);
     
+    // Event listener for form submission
     $('#taskForm').on('submit', handleAddTask);
-    //$('#taskForm').on('submit', validateForm);
-    
-    // 'use strict'
-    
-    // $('#taskForm').on('submit', function (event) {
-    //     if (!$('#taskForm').checkValidity()) {
-    //       event.preventDefault();
-    //       event.stopPropagation();
-    //       console.log("hello");
-    //     }
 
-    //     console.log("hello");
-    //     $('#taskForm').classList.add('was-validated');
-    //   }, false)
-
-    // TODO: Add an event listener to listen for the delete buttons. Use event delegation to call the `handleDeleteProject` function.
-    // Button delete - TODO
-    //$('').on('click', '.btn-delete-project', handleDeleteTask);
-
-    // ? Make lanes droppable
+    // Make lanes droppable
     $('.lane').droppable({
         accept: '.draggable',
         drop: handleDrop,
     });
     
+    // Date picker for due date field
     $('#taskDueDate').datepicker({
         changeMonth: true,
         changeYear: true,
     });
-
-    // $('#taskForm').validate({
-    //     rules: {
-    //       taskTitle: {
-    //         required: true
-    //       },
-    //       taskDueDate: {
-    //         required: true
-    //       },
-    //       action: "required"
-    //     },
-    //     messages: {
-    //         taskTitle: {
-    //         required: "Please enter some data",
-    //       },
-    //       taskDueDate: {
-    //         required: "Please enter some data",
-    //       },
-    //       action: "Please provide some data"
-    //     }
-    //   });
 });
